f5b4531851706055bf7e3f2e5fa5fe713d4a654c
fdina@home <fdina@home>
2025-12-25 12:16:32 -0500

    fix

---
+++ app.config.js
@@ -0,0 +1,28 @@
+import appJson from './app.json';
+
+export default ({ config }) => {
+  let iosBundleId = "com.faustinodina.mydirectory";
+  let androidPackage = "com.faustinodina.mydirectory";
+
+  const profile = process.env.EAS_BUILD_PROFILE;
+
+  if (profile === "development") {
+    iosBundleId = "com.faustinodina.mydirectory.dev";
+    androidPackage = "com.faustinodina.mydirectory.dev";
+  } else if (profile === "preview") {
+    iosBundleId = "com.faustinodina.mydirectory.preview";
+    androidPackage = "com.faustinodina.mydirectory.preview";
+  }
+
+  return {
+    ...appJson,
+    ios: {
+      ...appJson.ios,
+      bundleIdentifier: iosBundleId
+    },
+    android: {
+      ...appJson.android,
+      package: androidPackage
+    }
+  };
+};
