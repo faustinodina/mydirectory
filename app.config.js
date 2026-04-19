ced3973e0f5b1add52a2b0a4cec8b9f618b46e84
fdina@home <fdina@home>
2026-04-19 17:00:34 -0400

    fixes issue npx expo start not being able to start the app

--- app.config.js
+++ app.config.js
@@ -2,20 +2,21 @@ export default ({ config }) => {
   let iosBundleId = "com.faustinodina.mydirectory";
   let androidPackage = "com.faustinodina.mydirectory";
 
-  const profile = process.env.EAS_BUILD_PROFILE;
+  // this was done for being able to install a demo and a development versions simultaneously
+  // const profile = process.env.EAS_BUILD_PROFILE;
 
-  if (profile === "development") {
-    iosBundleId = "com.faustinodina.mydirectory.dev";
-    androidPackage = "com.faustinodina.mydirectory.dev";
-  }
-  else if (profile === "preview") {
-    iosBundleId = "com.faustinodina.mydirectory.preview";
-    androidPackage = "com.faustinodina.mydirectory.preview";
-  }
-  else if (profile === "production") {
-    iosBundleId = "com.faustinodina.mydirectory";
-    androidPackage = "com.faustinodina.mydirectory";
-  }
+  // if (profile === "development") {
+  //   iosBundleId = "com.faustinodina.mydirectory";
+  //   androidPackage = "com.faustinodina.mydirectory";
+  // }
+  // else if (profile === "preview") {
+  //   iosBundleId = "com.faustinodina.mydirectory";
+  //   androidPackage = "com.faustinodina.mydirectory";
+  // }
+  // else if (profile === "production") {
+  //   iosBundleId = "com.faustinodina.mydirectory";
+  //   androidPackage = "com.faustinodina.mydirectory";
+  // }
 
   return {
     ...config,
