e00a2ede95756547fcb1cc813332c537338a66ba
fdina@home <fdina@home>
2026-06-01 17:21:26 -0400

    adjusted header height in the modal add/edit screens

--- app/modal/_layout.tsx
+++ app/modal/_layout.tsx
@@ -1,10 +1,30 @@
 import { Stack } from "expo-router";
+import { Appbar, useTheme } from "react-native-paper";
 
 export default function ModalLayout() {
+  const theme = useTheme();
+
   return (
     <Stack
       screenOptions={{
         presentation: "modal",
+        header: ({ options, navigation, back }) => (
+          <Appbar.Header
+            statusBarHeight={0}
+            style={{ backgroundColor: theme.colors.primary }}
+          >
+            {back && (
+              <Appbar.BackAction
+                color={theme.colors.onPrimary}
+                onPress={() => navigation.goBack()}
+              />
+            )}
+            <Appbar.Content
+              title={options.title ?? ''}
+              color={theme.colors.onPrimary}
+            />
+          </Appbar.Header>
+        ),
       }}
     />
   );
